75cbbe8e9091b0257418b9e91e30fa4e6a836b07
min <minwang.bus@gmail.com>
2021-05-24 00:42:29 -0400

    work on frontend debug

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://dev:OvYM90ANDkqR5Qsm@cluster0.y2o4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority://dev:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority'
-          
+    mongoURI: 'mongodb+srv://dev:OvYM90ANDkqR5Qsm@cluster0.y2o4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
   }
